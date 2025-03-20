390314c60c5c1605de751df55281dfb7c4f6ace5
Babuleh78 <jdbdbsbbfdh@gmail.com>
2025-03-20 17:17:58 +0300

    помнят руки

--- kanban/src/column/Column.jsx
+++ kanban/src/column/Column.jsx
@@ -2,26 +2,79 @@
 import './column.css';
 import AddBtn from '../addBtn/addBtn';
 import Sticker from '../sticker/sticker';
-import React, { useState } from 'react';
+import React, { useState, useRef  } from 'react';
 function Column(props) {
-  const [stickers, setStickers] = useState([]);
+  const [stickers, setStickers] = useState([])
+  const [isDragging, setIsDragging] = useState([])
+  const [position, setPosition] = useState([]) // Позиция мыши
+  const [offset, setOffset] = useState([]) //Смещение мыши относительно левого верхнего в начале движения
+  const columnRef = useRef(null)
 
   const addSticker = () => {
-    const newItem = `Sticker ${stickers.length + 1}`
-    setStickers([...stickers, newItem])
+    const newItem = `Sticker ${stickers.length + 1}`;
+    setStickers([...stickers, newItem]);
+  };
+
+  const handleMouseDown = (e) => { //Пользователь нажал на мышь
+    if(e.button === 0){
+      return
+    }
+    setIsDragging(true)
+    const { pageX, pageY} = e
+    const {offsetLeft, offsetTop} = columnRef.current
+    setOffset({
+      x: pageX - offsetLeft,
+      y: pageY - offsetTop
+    })
+  }
+
+  const hadleMouseMove = (e) =>{ //Пользователь двигает мышью
+    if(isDragging){
+      const{pageX, pageY} = e
+
+      const newX = pageX - offset.x
+      const newY = pageY - offset.y
+      setPosition({x: newX, y: newY})
+    }
+  }
+
+  const handleMouseUp = () => {
+    setIsDragging(false);
   };
 
   return (
-    <div className="column-container" id = {props.columnName}> 
+    <div
+      className="column-container"
+      id={props.columnName}
+      draggable={isDragging}
+      ref = {columnRef}
+      style={{
+        position: 'absolute',
+        left: `${position.x}px`,
+        top: `${position.y}px`,
+        cursor: isDragging ? 'grabbing' : 'grab',
+      }}
+
+      onMouseDown={handleMouseDown}
+      onMouseMove={hadleMouseMove}
+      onMouseUp={handleMouseUp}
+      onMouseLeave={handleMouseUp} 
+      onContextMenu={(e) => e.preventDefault()}
+  
+    >
       <div className="columnName">{props.columnName}</div>
       <div className="Column">
-        <AddBtn onClick = {addSticker}/> 
+        <AddBtn onClick={addSticker} />
         {stickers.map((sticker, index) => (
-          <Sticker key={index} name={sticker} />
+          <Sticker
+            key={index}
+            title={"Title"}
+            text={"Text"}
+            stickerId={`Sticker-${index}`}
+          />
         ))}
       </div>
-     
-  </div>
+    </div>
   );
 }
 
--- kanban/src/sticker/sticker.jsx
+++ kanban/src/sticker/sticker.jsx
@@ -3,42 +3,12 @@ import './sticker.css'
 const Sticker = React.memo(function Sticker(props) {
    console.log(props)
    const { text, stickerId, title } = props;
-    function dragStartHandle(e) {
-      e.dataTransfer.setData('text/plain', e.target.id); // Сохраняем ID элемента
-      e.target.style.opacity = '0.4'; // Визуальный эффект
-    }
-  
-    function dragLeaveHandle(e) {
-      e.target.style.backgroundColor = ''; // Убираем подсветку
-    }
-  
-    function dragEndHandle(e) {
-      e.target.style.opacity = '1'; // Восстанавливаем визуальный эффект
-    }
-  
-    function dragOverHandle(e) {
-      e.preventDefault(); // Разрешаем сброс
-      e.dataTransfer.dropEffect = 'move'; // Указываем тип операции
-    }
-  
-    function dragDropHandle(e) {
-      e.preventDefault();
-      const stickerId = e.dataTransfer.getData('text/plain'); // Получаем ID элемента
-      const stickerElement = document.getElementById(stickerId); // Находим элемент
-      e.target.appendChild(stickerElement); // Добавляем элемент в новый контейнер
-      stickerElement.style.opacity = '1'; // Восстанавливаем визуальный эффект
-    }
-  
+    
     return (
       <div
         id={stickerId} // Уникальный ID для каждого стикера
         className="l"
         draggable={true}
-        onDragStart={(e) => dragStartHandle(e)}
-        onDragLeave={(e) => dragLeaveHandle(e)}
-        onDragEnd={(e) => dragEndHandle(e)}
-        onDragOver={(e) => dragOverHandle(e)}
-        onDrop={(e) => dragDropHandle(e)}
       >
         <a>
           <h2>{title}</h2>
