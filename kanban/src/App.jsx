570d7219442c1bc67e03853acbeeba47bf66f705
Babuleh78 <jdbdbsbbfdh@gmail.com>
2025-03-20 16:37:43 +0300

    Обкашляли вопросики с рендером

--- kanban/src/App.jsx
+++ kanban/src/App.jsx
@@ -1,16 +1,16 @@
 
 import './App.css';
 import Column from './column/column';
-import Sticker from './sticker/sticker';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <div className="App-header">
          <Column columnName = "To Do" />
          <Column columnName = "Doing" />
          <Column columnName = "Done" />
          
-      </header>
+         
+      </div>
     </div>
   );
 }
--- kanban/src/column/Column.jsx
+++ kanban/src/column/Column.jsx
@@ -4,22 +4,20 @@ import AddBtn from '../addBtn/addBtn';
 import Sticker from '../sticker/sticker';
 import React, { useState } from 'react';
 function Column(props) {
-
   const [stickers, setStickers] = useState([]);
 
   const addSticker = () => {
-    const newItem = `Sticker ${stickers.length + 1}`; //
-    setStickers([...stickers, newItem]); 
-    console.log("Шаш", stickers.length)
+    const newItem = `Sticker ${stickers.length + 1}`
+    setStickers([...stickers, newItem])
   };
 
   return (
     <div className="column-container" id = {props.columnName}> 
       <div className="columnName">{props.columnName}</div>
       <div className="Column">
-        <AddBtn onClick = {addSticker}/>
+        <AddBtn onClick = {addSticker}/> 
         {stickers.map((sticker, index) => (
-          <Sticker key={index} text={sticker} /> // Отображение каждого стикера
+          <Sticker key={index} name={sticker} />
         ))}
       </div>
      
